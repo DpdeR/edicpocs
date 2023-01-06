35e5083a1f94a6138c0b34393e1077c8a9e6df22
David de Rijke <dpderijke@gmail.com>
2023-01-06 15:41:48 +0100

    Add template MVC webapp

---
+++ MVC-webRole/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_webRole.ServiceReference1;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVC_webRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Data() 
+        {
+            var client = new Service1Client();
+            var data = client.GetDataUsingDataContract(
+                new CompositeType() { StringValue = "Hello from MVC ", BoolValue = true });
+            var dataList = new List<CompositeType>() { data };
+            return View(dataList);
+        }
+    }
+}
